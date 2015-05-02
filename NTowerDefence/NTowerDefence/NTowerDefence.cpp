25505ffbf32f8dc43a88fd8e320351c302d10c40
Lukasz Kosiak <kosiak.lukasz@gmail.com>
2015-05-02 16:05:29 +0200

    compilable

--- NTowerDefence/NTowerDefence/NTowerDefence.cpp
+++ NTowerDefence/NTowerDefence/NTowerDefence.cpp
@@ -1,11 +1,63 @@
-// NTowerDefence.cpp : Defines the entry point for the console application.
-//
+#include <stdio.h>
+#include <stdlib.h>
 
-#include "stdafx.h"
+#include <GL/glew.h>
+#include <GLFW\glfw3.h>
 
+GLFWwindow* window;
 
-int _tmain(int argc, _TCHAR* argv[])
+//#include <glm/glm.hpp>
+//using namespace glm;
+
+int main(void)
 {
+	// Initialise GLFW
+	if (!glfwInit())
+	{
+		fprintf(stderr, "Failed to initialize GLFW\n");
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Open a window and create its OpenGL context
+	window = glfwCreateWindow(1024, 768, "NTowerDefence", NULL, NULL);
+	if (window == NULL){
+		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible.\n");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Initialize GLEW
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		return -1;
+	}
+
+	// Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	// Dark blue background
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+	do{
+
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} // Check if the ESC key was pressed or the window was closed
+	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+	glfwWindowShouldClose(window) == 0);
+
+
+	glfwTerminate();
+
 	return 0;
 }
 
