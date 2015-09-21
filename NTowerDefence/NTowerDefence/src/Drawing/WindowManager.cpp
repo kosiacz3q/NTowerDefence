49070ab32d56333842ae09b0dd77ee3241d88952
unknown <pawlaksebastian@hotmail.com>
2015-09-21 12:56:51 +0200

    linki

--- NTowerDefence/NTowerDefence/src/Drawing/WindowManager.cpp
+++ NTowerDefence/NTowerDefence/src/Drawing/WindowManager.cpp
@@ -34,7 +34,7 @@ void WindowManager::initGLFW()
 
 	glfwWindowHint(GLFW_SAMPLES, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	
